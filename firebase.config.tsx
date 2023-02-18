6b4bb150e7b1402e442169a52f9f5f2127308fea
Maxthurr <maxime.robert7806@gmail.com>
2023-02-18 23:14:32 +0100

    Added firebase

---
+++ firebase.config.tsx
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBGWHmnBysbNfhFjXJ2-A8BSn9U9M3rrNc",
+    authDomain: "mypronote-27044.firebaseapp.com",
+    projectId: "mypronote-27044",
+    storageBucket: "mypronote-27044.appspot.com",
+    messagingSenderId: "501536549682",
+    appId: "1:501536549682:web:aa3072835b99e4f58c452b",
+    measurementId: "G-GK93SFCLG5"
+  };
