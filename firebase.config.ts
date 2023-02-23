3a9e3db8e3d2b59b1b8edecdd7d207a7c26735d2
Maxthurr <maxime.robert7806@gmail.com>
2023-02-23 21:01:12 +0100

    Fix

--- firebase.config.ts
+++ firebase.config.ts
@@ -1,7 +1,4 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
+export const firebaseConfig = {
     apiKey: "AIzaSyBGWHmnBysbNfhFjXJ2-A8BSn9U9M3rrNc",
     authDomain: "mypronote-27044.firebaseapp.com",
     projectId: "mypronote-27044",
@@ -10,6 +7,3 @@ const firebaseConfig = {
     appId: "1:501536549682:web:aa3072835b99e4f58c452b",
     measurementId: "G-GK93SFCLG5"
 };
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { firebaseConfig } from 'firebase.config'
+import { Firestore, getFirestore } from 'firebase/firestore'
+import { initializeApp } from 'firebase/app'
 
+export var db: Firestore;
 export default function App({ Component, pageProps }: AppProps) {
+  const app = initializeApp(firebaseConfig);
+  db = getFirestore(app);
   return <Component {...pageProps} />
 }
